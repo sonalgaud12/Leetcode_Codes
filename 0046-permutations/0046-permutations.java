cd5822bbfb7ae0f48d31ffe915c42ca4cfadb266
sonalgaud12 <82362774+sonalgaud12@users.noreply.github.com>
2024-10-05 19:31:40 +0530

    Time: 2 ms (45.16%), Space: 44.9 MB (6.60%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,12 +1,11 @@
 class Solution {
-    
     private List<List<Integer>> result = new ArrayList<>();
     private Set<Integer> st = new HashSet<>();
     int n;
     
     public void solve(List<Integer> temp, List<Integer> nums){
-        if(temp.size()==n){
-            result.add(new ArrayList<>(temp));
+        if(temp.size() == n){
+            result.add(new ArrayList<>(temp)); // add new list to avoid refernce
             return;
         }
         
@@ -17,9 +16,9 @@ class Solution {
                 
                 solve(temp, nums);
                 
-                // backtraking
                 st.remove(nums.get(i));
-                temp.remove(nums.get(i));
+                temp.remove(temp.size()-1);
+                
             }
         }
     }
@@ -28,12 +27,11 @@ class Solution {
         n = nums.length;
         List<Integer> temp = new ArrayList<>();
         List<Integer> numList = new ArrayList<>();
-            for (int num : nums) {
-                numList.add(num);
-            }
-
-        solve(temp,numList);
-        return result;
         
+        for(int num: nums){
+            numList.add(num);
+        }
+        solve(temp, numList);
+        return result;
     }
 }
