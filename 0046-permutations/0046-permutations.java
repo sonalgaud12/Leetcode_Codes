9c1843675d689f54cf75c5a038a52854e6e2905a
sonalgaud12 <82362774+sonalgaud12@users.noreply.github.com>
2024-10-05 20:03:50 +0530

    Time: 1 ms (94.84%), Space: 44.5 MB (50.96%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,37 +1,41 @@
 class Solution {
+    
+    // Approach 2
     private List<List<Integer>> result = new ArrayList<>();
-    private Set<Integer> st = new HashSet<>();
     int n;
     
-    public void solve(List<Integer> temp, List<Integer> nums){
-        if(temp.size() == n){
-            result.add(new ArrayList<>(temp)); // add new list to avoid refernce
+    public void solve(int idx, List<Integer> nums){
+        if(idx==n){
+            result.add(new ArrayList(nums));
             return;
         }
         
-        for(int i=0;i<n;i++){
-            if(!st.contains(nums.get(i))){
-                temp.add(nums.get(i));
-                st.add(nums.get(i));
-                
-                solve(temp, nums);
-                
-                st.remove(nums.get(i));
-                temp.remove(temp.size()-1);
-                
-            }
+        for(int i=idx;i<n;i++){
+            
+            swap(nums,i, idx);
+            solve(idx+1, nums);
+            swap(nums,i,idx);
+            
         }
     }
+    
+    public void swap(List<Integer> nums,int i, int j){
+        int temp= nums.get(i);
+        nums.set(i, nums.get(j));
+        nums.set(j,temp);
+        
+}
     public List<List<Integer>> permute(int[] nums) {
         
         n = nums.length;
-        List<Integer> temp = new ArrayList<>();
         List<Integer> numList = new ArrayList<>();
-        
         for(int num: nums){
             numList.add(num);
         }
-        solve(temp, numList);
+        solve(0, numList);
         return result;
+        
+        
+        
     }
 }
