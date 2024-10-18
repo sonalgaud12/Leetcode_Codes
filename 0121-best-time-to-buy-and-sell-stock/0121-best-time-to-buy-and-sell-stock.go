89e54dc99fd2d14adf3c794677e2e7b0c0eed647
sonalgaud12 <82362774+sonalgaud12@users.noreply.github.com>
2024-10-18 21:10:01 +0530

    Time: 14 ms (99.63%), Space: 9.9 MB (18.95%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -1,18 +1,18 @@
 func maxProfit(prices []int) int {
-    if len(prices) == 0{
+    if len(prices)==0{
         return 0
     }
     
     max := 0
+    sell := prices[0]
     buy := prices[0]
-    sell:= prices[0]
     
-    for i:= 0 ; i < len(prices); i++{
-        buy = int(math.Min(float64(buy), float64(prices[i])))
+    for i:=0; i<len(prices);i++{
+        buy = int(math.Min(float64(buy),float64(prices[i])))
         sell = int(math.Max(float64(buy), float64(prices[i])))
-        
-        max = int(math.Max(float64(max),float64(sell-buy)))
+        max = int(math.Max(float64(max), float64(sell-buy)))
     }
     
-    return max;
+    return max
+    
 }
