1b2085a29b4729356268efbe46ccdb074fa91d66
sonalgaud12 <82362774+sonalgaud12@users.noreply.github.com>
2024-10-18 20:54:04 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (7.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,16 +1,16 @@
 func twoSum(nums []int, target int) []int {
-    res := make([]int, 2)
-    mp := make(map[int]int)
+    res := make([]int, 2) // array
+    mp := make(map[int]int) // map
     
     for i:= 0; i< len(nums);i++{
-        cp := target - nums[i]
+        cp := target - nums[i] // getting the complement 
         
-        if index, found := mp[cp]; found{
-            res[0] =i
+        if index, found := mp[cp]; found{ // checking if the complement avaliable in the map
+            res[0] =i // if yes then i will be stored 0 , 1 index of res
             res[1] = index
             return res
         }
-        mp[nums[i]] = i
+        mp[nums[i]] = i // if not found then stored in the map
     }
     return res
 }
